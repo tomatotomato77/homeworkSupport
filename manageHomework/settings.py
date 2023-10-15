865ba7bd8fb94301e270b15f7a76d67c87a73c0b
tomatotomato77 <nobrac.ra@gmail.com>
2023-10-16 07:39:58 +0900

    requirements

--- manageHomework/settings.py
+++ manageHomework/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_cleanup'
 ]
 
 MIDDLEWARE = [
@@ -79,7 +80,7 @@ WSGI_APPLICATION = 'manageHomework.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgreql_psycopg2',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
