ba44af7dc9e98f80cff559beef10aa4ea772da0f
tomatotomato77 <nobrac.ra@gmail.com>
2023-10-17 12:54:10 +0900

    settings

--- manageHomework/settings.py
+++ manageHomework/settings.py
@@ -126,8 +126,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
+#CSRF_COOKIE_SECURE = True
+#SESSION_COOKIE_SECURE = True
 try:
     from .local_settings import *
 except:
