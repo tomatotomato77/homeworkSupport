17108e4771cff6e820fb07a0238320b4c2476581
tomatotomato77 <nobrac.ra@gmail.com>
2023-10-17 20:32:03 +0900

    local

--- manageHomework/settings.py
+++ manageHomework/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o%a8*8ullc-_d_5n@#zr)uch36g=d)728=foooah@o(u%6-6f='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
