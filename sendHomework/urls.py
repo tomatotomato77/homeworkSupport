c6712c0d17c3cafff42575176f813c0fcede2663
tomatotomato77 <nobrac.ra@gmail.com>
2023-10-17 17:06:40 +0900

    settings add media_url

--- sendHomework/urls.py
+++ sendHomework/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path("send/",views.indexView,name="send"),
     path("showhistry/",views.showView.as_view(),name="showhistory"),
