4a054075e4200452fb9072a666bff63bfafe47dc
tomatotomato77 <nobrac.ra@gmail.com>
2023-10-19 11:04:37 +0900

    requirements

--- manageHomework/settings.py
+++ manageHomework/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from django.core.management.utils import get_random_secret_key
-import os
 
 SECRET_KEY = get_random_secret_key()  
 
